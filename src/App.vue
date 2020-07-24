e87b26ded11355ef206b6ca705ea576c62ba6bb5
agheadley <agheadley@gmail.com>
2020-07-24 16:16:53 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-app-bar app>Monitor v1</v-app-bar>
+      <v-main>
+        <v-container fluid>
+          <Monitor/>
+        </v-container>
+      </v-main>
+      <v-footer app>AGH</v-footer>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Monitor from "./components/Monitor";
+
+export default {
+  name: "App",
+  components: {
+    Monitor
+  }
+};
+</script>
